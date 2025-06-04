1a81e72c253bef2bc68e4f779fa4c55143804812
Revisto <theRevisto@gmail.com>
2025-06-04 23:30:47 +0330

    Update copyright year from 2024 to 2025 in multiple source files

--- src/services/preset_service.py
+++ src/services/preset_service.py
@@ -1,6 +1,6 @@
 # services/preset_service.py
 #
-# Copyright 2024 revisto
+# Copyright 2025 revisto
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
